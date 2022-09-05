726be33b990a609d38d370839c80975f7b3287c7
pongsatt <pongsatt@gmail.com>
2022-09-05 22:14:12 +0700

    sticky

--- regionalRollout.js
+++ regionalRollout.js
@@ -5,10 +5,40 @@ class RegionalRolloutStrategy extends Strategy {
     super('RegionalRollout');
   }
 
-  isEnabled(parameters, context) {
-    const result = Math.random() < parameters.percent / 100;
-    console.log('RegionalRollout - evaluate rollout: ', parameters.percent, ' result: ', result);
+  #CACHE = {};
+  
+  withStickiness(parameters, context, getValue) {
+    const key = context['sessionId'];
+
+    if (!key) {
+      // when sessionId is missing from the context, evaluate the function
+      return getValue();
+    }
+
+    // retrieve previous value from cache
+    const stickyResult = this.#CACHE[key];
+
+    if (stickyResult !== undefined) {
+      // in the cache
+      console.log('RegionRollout - evaluate stickiness: ', parameters.stickiness, ' stickyResult: ', stickyResult);
+      return stickyResult;
+    }
+
+    // not in the cache, evaluate the function
+    const result = getValue();
+
+    // store value in cache
+    this.#CACHE[key] = result;
+
     return result;
+  }
+
+  isEnabled(parameters, context) {
+    return this.withStickiness(parameters, context, () => {
+      const result = Math.random() < parameters.percent / 100;
+      console.log('RegionalRollout - evaluate rollout: ', parameters.percent, ' result: ', result);
+      return result;
+    });
   }
 }
 
